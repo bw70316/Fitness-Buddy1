5338f0f4c0ccca69016a6501ca981c4a1629a6c9
brian wisnowski <brian wisnowski>
2017-04-06 18:30:46 -0400

    index, css, apis, forms...

---
+++ data.js
@@ -0,0 +1,66 @@
+var athleteList = [{"athlete": "Michael Jordan",
+ "age": "52",
+ "exercise": "Gym Workouts",
+ "gender": "Male",
+ "address": "1901 Oak Tree Dr., Chapel Hill, NC 27517"
+ }, {
+ "athlete": "Mia Hamm",
+ "age": "45" ,
+ "exercise": "Running",
+ "gender": "Female",
+ "address": "1521 E Franklin St Chapel Hill, NC 27514"
+ }, {
+ "athlete": "Wayne Gretzky",
+ "age": "56" ,
+ "exercise": "Gym Workouts",
+ "gender": "Male",
+  "address": "200 Davie Rd, Carrboro, NC 27510"
+ }, {
+ "athlete": "Simone Biles",
+ "age": "20" ,
+ "exercise": "Running",
+ "gender": "Female",
+  "address": "441 Claremont Dr, Chapel Hill, NC 27516"
+ 
+},  {"athlete": "Joel Berry II",
+ "age": "22" ,
+ "exercise": "Climbing",
+ "gender": "Male",
+  "address": "105 Telluride Trail Chapel Hill, NC 27514"
+ },   {"athlete": "Diana Taurasi",
+ "age": "34" ,
+ "exercise": "Running",
+ "gender": "Female",
+  "address": "7016 Turkey Farm Rd, Chapel Hill, NC 27514"
+},   {"athlete": "Russell Westbrook",
+ "age": "28" ,
+ "exercise": "Gym Workouts",
+ "gender": "Male",
+  "address": "1704 Michaux Rd Chapel Hill, NC 27514"
+
+},   {"athlete": "Alex Morgan",
+ "age": "27" ,
+ "exercise": "Climbing",
+ "gender": "Female",
+  "address": "109 Bolinwood Dr, Chapel Hill, NC 27514"
+},   {"athlete": "Kobe Bryant",
+ "age": "38" ,
+ "exercise": "Running",
+ "gender": "Male",
+  "address": "413 N Columbia St. Chapel Hill, NC 27516"
+ },   {"athlete": "Serena Williams",
+ "age": "35" ,
+ "exercise": "Running",
+ "gender": "Female",
+  "address": "107 Hayworth Dr, Chapel Hill, NC 27517"
+ },   {"athlete": "James Harden",
+ "age": "27" ,
+ "exercise": "Gym Workouts",
+ "gender":"Male",
+ "address": "100 Adams Way, Chapel Hill, NC 27516"
+ },   {"athlete": "Venus Williams",
+ "age": "36" ,
+ "exercise": "Climbing",
+ "gender": "Female"
+  "address": "1401 Mason Farm Road, Chapel Hill, NC 27514"
+ }]; 
